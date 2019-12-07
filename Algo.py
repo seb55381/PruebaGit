c43f7f99559be5a7c87d0cc317c8fc743c1e15b2
Sebastian Zuzunaga <seb55381@gmail.com>
2019-12-06 19:58:38 -0600

    Algo chido pe

---
+++ Algo.py
@@ -0,0 +1,12 @@
+i = 0
+while i != 5:
+    print("Hola xd")
+    i += 1
+
+A = input("Ingresa una letra: ")
+A = A.lower()
+print(A)
+
+S = input("Ingresa otra cosa: ")
+S = S.replace(" ","")
+print(S)
